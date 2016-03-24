a84d0b1d657f09ac3d8bc1041807cd3890fba1ca
RafaelSPBarbosa <rafaspb11@gmail.com>
2016-03-24 20:36:25 -0300

    Versão inicial do jogo
    Atualizado na unity 5.3.2f1 - PORFAVOR USAR A MESMA VERSÃO OU O GIT VAI
    BUGAR!
    
    - Movimentação básica de câmera.
    - Mecânica de selecionar e des-selecionar uma unidade.
    - Movimento básico de Tile para Tile.
    - Restrição de movimento para cada tipo de infantaria.

---
+++ EstrategiaTabuleiro/Assets/Scripts/UnitManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitManager : MonoBehaviour {
+
+    //Definição de Variáveis
+    public int MaxActions, curActions;
+    public bool Selected;
+    public GameObject UnitSelectedIndicator;
+    //Definição de Variáveis
+
+    public void RecarregarAcoes()
+    {
+
+        curActions = MaxActions;
+    }
+
+    void OnMouseDown()
+    {
+        if (Selected == true)
+        {
+            DeSelectUnit();
+        }
+        else
+        {
+            //O player só pode selecionar a unidade caso ela tenha mais de uma ação, caso contrário, não poderá nem sequer selecionar, quanto menos mover.
+            if (curActions >= 1)
+            {
+                // Aqui como uma outra unidade pode estar selecionada, eu procuro todas as unidades da mesma forma que eu fiz ali em cima, e então des-seleciono elas
+                GameObject[] AllFriendlyUnits = GameObject.FindGameObjectsWithTag("Unit");
+
+                for (int i = 0; i < AllFriendlyUnits.Length; i++)
+                {
+                    AllFriendlyUnits[i].GetComponent<UnitManager>().Selected = false;
+                }
+
+                //Com nenhuma outra unidade selecionada, seleciono agora esta unidade sem risco de ter 2 unidades selecionadas ao mesmo tempo.
+                Selected = true;
+            }
+        }
+    }
+
+    void DeSelectUnit()
+    {
+        //Caso o player seja selecionado e "Selected" seja verdadeiro, tornar falso,
+        Selected = false;
+
+        // Então encontro e coloco todas os objetos com tag "Tile" dentro da variavel AllTiles
+        GameObject[] AllTiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        // Com um For Loop eu entro no script "TileManager" de cada um dos objetos dentro de "AllTiles" e torno a variável "SelectedUnit" deles igual a nullo ( Nada )
+        for (int i = 0; i < AllTiles.Length; i++)
+        {
+            AllTiles[i].GetComponent<TileManager>().SelectedUnit = null;
+        }
+    }
+
+    void Update()
+    {
+        //Aqui estou apenas ligando e desligando o efeito de uma unidade selecionada
+        if(Selected == true)
+        {
+            UnitSelectedIndicator.SetActive(true);
+        }
+        else
+        {
+            UnitSelectedIndicator.SetActive(false);
+        }
+
+        //Caso as ações do jogador acabem, a unidade é desselecionada
+        if( curActions <= 0 && Selected == true)
+        {
+            DeSelectUnit();
+        }
+    }
+}
